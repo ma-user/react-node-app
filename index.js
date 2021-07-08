df7a76aaf419bd0b24efb52e023b8fd23eb6c325
ma-user <73891478+ma-user@users.noreply.github.com>
2021-07-08 13:44:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ app.delete('/delete', (req, res) => {
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('build'));
-    app.get('/', (req, res) => {
+    app.get('/*', (req, res) => {
         req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
     })
 }
